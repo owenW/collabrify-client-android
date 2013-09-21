9e97e7c035e88a61f56822c4231477eb5158c2e9
OwenBla <zhichao18@gmail.com>
2013-09-21 19:41:17 -0400

    1. imported MovesProtos. 2. get edited text
    Note: need to read more on User.java

--- gen/com/example/wewrite/R.java
+++ gen/com/example/wewrite/R.java
@@ -28,7 +28,7 @@ public final class R {
         public static final int action_settings=0x7f080004;
         public static final int broadcast_button=0x7f080003;
         public static final int redo_button=0x7f080001;
-        public static final int to_broadcast=0x7f080000;
+        public static final int text_to_broadcast=0x7f080000;
         public static final int undo_button=0x7f080002;
     }
     public static final class layout {
--- src/com/example/wewrite/MainActivity.java
+++ src/com/example/wewrite/MainActivity.java
@@ -1,18 +1,53 @@
 package com.example.wewrite;
+import java.util.Vector;
 
-import android.os.Bundle;
+import org.w3c.dom.UserDataHandler;
+
+import com.example.wewrite.MovesProtos.Move;
+
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity
 {
-
+  private EditText text_to_broadcast;
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    
+    //get buttons
+    Button redo_button = (Button) findViewById(R.id.redo_button);
+    Button undo_button = (Button) findViewById(R.id.undo_button);
+    
+    //get text
+    text_to_broadcast = (EditText) findViewById(R.id.text_to_broadcast);
+    
+    
+    
+    
+        
   }
+  
+  View.OnClickListener redo_handler = new View.OnClickListener()
+  {
+    @Override
+    public void onClick(View v)
+    {
+      // TODO Auto-generated method stub
+      
+    }
+  };
 
 
   @Override
